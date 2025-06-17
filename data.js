2c61221e2c6f1b63ac6c120b2c0c53d7d572c9c4
ritesh <ritesh25033@gmail.com>
2025-06-17 15:59:26 +0530

    up

---
+++ data.js
@@ -0,0 +1,80 @@
+const data = [
+  {
+    img: "./assets/images/avatar-mark-webber.png",
+    info: {
+      name: "Mark Webber",
+      action: "reacted to your recent post",
+      postName: "My first tournament today!",
+      time: "1m ago",
+      privateMessage: "",
+      picture: "",
+    },
+  },
+  {
+    img: "./assets/images/avatar-angela-gray.png",
+    info: {
+      name: "Angela Gray",
+      action: "followed you",
+      postName: "",
+      time: "5m ago",
+      privateMessage: "",
+      picture: "",
+    },
+  },  
+  {
+    img: "./assets/images/avatar-jacob-thompson.png",
+    info: {
+      name: "Jacob Thompson",
+      action: "has joined your group",
+      postName: "Chess Club",
+      time: "1 day ago",
+      privateMessage: "",
+      picture: "",
+    },
+  },
+  {
+    img: "./assets/images/avatar-rizky-hasanuddin.png",
+    info: {
+      name: "Rizky Hasanuddin",
+      action: "sent you a private message",
+      postName: "",
+      time: "5 days ago",
+      privateMessage:
+        "Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I'm already having lots of fun and improving my game.",
+      picture: "",
+    },
+  },
+  {
+    img: "./assets/images/avatar-kimberly-smith.png",
+    info: {
+      name: "Kimberly Smith",
+      action: "commented on your picture",
+      postName: "",
+      time: "1 week ago",
+      privateMessage: "",
+      picture: "./assets/images/image-chess.png",
+    },
+  },
+  {
+    img: "./assets/images/avatar-nathan-peterson.png",
+    info: {
+      name: "Nathan Peterson",
+      action: "reacted to your recent post",
+      postName: "5 end-game strategies to increase your win rate",
+      time: "2 weeks ago",
+      privateMessage: "",
+      picture: "",
+    },
+  },
+  {
+    img: "./assets/images/avatar-anna-kim.png",
+    info: {
+      name: "Anna Kim",
+      action: "left the group",
+      postName: "Chess Club",
+      time: "2 weeks ago",
+      privateMessage: "",
+      picture: "",
+    },
+  },
+];
---
+++ script.js
@@ -0,0 +1,103 @@
+// Get DOM elements
+const notificationsList = document.getElementById('list');
+const markAllButton = document.querySelector('.mark-all-button');
+const notificationsCounter = document.querySelector('.notifications-counter');
+
+// Track unread notifications (first 3 are unread initially)
+let unreadCount = 3;
+const unreadNotifications = new Set([0, 1, 2, 3]); // Mark first 4 as unread
+
+// Function to render notifications
+function renderNotifications() {
+    notificationsList.innerHTML = '';
+    
+    data.forEach((notification, index) => {
+        const listItem = document.createElement('li');
+        listItem.className = `notification-item ${unreadNotifications.has(index) ? 'new-notification' : ''}`;
+        
+        // Create notification HTML
+        let notificationHTML = `
+            <img src="${notification.img}" alt="${notification.info.name}" class="notification-avatar">
+            <div class="notification-content">
+                <div class="notification-infos">
+                    <a href="#" class="profile-link">${notification.info.name}</a>
+                    <span class="notification-text">${notification.info.action}</span>
+                    ${notification.info.postName ? `<a href="#" class="notification-link-post">${notification.info.postName}</a>` : ''}
+                    ${unreadNotifications.has(index) ? '<span class="notification-dot"></span>' : ''}
+                </div>
+                <div class="notification-time">${notification.info.time}</div>
+                ${notification.info.privateMessage ? `<div class="notification-text-private-message">${notification.info.privateMessage}</div>` : ''}
+            </div>
+        `;
+        
+        // Add picture if exists
+        if (notification.info.picture) {
+            notificationHTML += `<img src="${notification.info.picture}" alt="Chess" class="notification-picture">`;
+        }
+        
+        listItem.innerHTML = notificationHTML;
+        
+        // Add click event for private messages
+        if (notification.info.privateMessage) {
+            listItem.addEventListener('click', () => {
+                const privateMessage = listItem.querySelector('.notification-text-private-message');
+                privateMessage.classList.toggle('show');
+                
+                // Mark as read when clicked
+                if (unreadNotifications.has(index)) {
+                    markAsRead(index);
+                }
+            });
+        } else {
+            // Mark as read when clicked (for non-private messages)
+            listItem.addEventListener('click', () => {
+                if (unreadNotifications.has(index)) {
+                    markAsRead(index);
+                }
+            });
+        }
+        
+        notificationsList.appendChild(listItem);
+    });
+    
+    updateCounter();
+}
+
+// Function to mark individual notification as read
+function markAsRead(index) {
+    unreadNotifications.delete(index);
+    unreadCount = unreadNotifications.size;
+    renderNotifications();
+}
+
+// Function to mark all notifications as read
+function markAllAsRead() {
+    unreadNotifications.clear();
+    unreadCount = 0;
+    renderNotifications();
+}
+
+// Function to update counter
+function updateCounter() {
+    notificationsCounter.textContent = unreadCount;
+    if (unreadCount === 0) {
+        notificationsCounter.style.display = 'none';
+    } else {
+        notificationsCounter.style.display = 'inline-block';
+    }
+}
+
+// Event listeners
+markAllButton.addEventListener('click', markAllAsRead);
+
+// Add hover effects for profile links and post links
+document.addEventListener('click', (e) => {
+    if (e.target.matches('.profile-link') || e.target.matches('.notification-link-post')) {
+        e.preventDefault();
+        // Navigate to # (placeholder)
+        window.location.hash = '#';
+    }
+});
+
+// Initial render
+renderNotifications();
