527cd10a14220f7991ff8abdc4ac2e8807ce1dac
ritesh <ritesh25033@gmail.com>
2025-06-19 12:04:42 +0530

    updtaed

--- data.js
+++ data.js
@@ -1,64 +1,80 @@
 const data = [
-    {
-        img: "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?w=50&h=50&fit=crop&crop=face",
-        info: {
-            name: "Mark Webber",
-            action: "reacted to your recent post",
-            postName: "My first tournament today!",
-            time: "1m ago"
-        }
+  {
+    img: "./assets/images/avatar-mark-webber.png",
+    info: {
+      name: "Mark Webber",
+      action: "reacted to your recent post",
+      postName: "My first tournament today!",
+      time: "1m ago",
+      privateMessage: "",
+      picture: "",
     },
-    {
-        img: "https://images.unsplash.com/photo-1494790108755-2616b612b786?w=50&h=50&fit=crop&crop=face",
-        info: {
-            name: "Angela Gray",
-            action: "followed you",
-            time: "5m ago"
-        }
+  },
+  {
+    img: "./assets/images/avatar-angela-gray.png",
+    info: {
+      name: "Angela Gray",
+      action: "followed you",
+      postName: "",
+      time: "5m ago",
+      privateMessage: "",
+      picture: "",
     },
-    {
-        img: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?w=50&h=50&fit=crop&crop=face",
-        info: {
-            name: "Jacob Thompson",
-            action: "has joined your group",
-            postName: "Chess Club",
-            time: "1 day ago"
-        }
+  },  
+  {
+    img: "./assets/images/avatar-jacob-thompson.png",
+    info: {
+      name: "Jacob Thompson",
+      action: "has joined your group",
+      postName: "Chess Club",
+      time: "1 day ago",
+      privateMessage: "",
+      picture: "",
     },
-    {
-        img: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=50&h=50&fit=crop&crop=face",
-        info: {
-            name: "Rizky Hasanuddin",
-            action: "sent you a private message",
-            time: "5 days ago",
-            privateMessage: "Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I'm already having lots of fun and improving my game."
-        }
+  },
+  {
+    img: "./assets/images/avatar-rizky-hasanuddin.png",
+    info: {
+      name: "Rizky Hasanuddin",
+      action: "sent you a private message",
+      postName: "",
+      time: "5 days ago",
+      privateMessage:
+        "Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I'm already having lots of fun and improving my game.",
+      picture: "",
     },
-    {
-        img: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?w=50&h=50&fit=crop&crop=face",
-        info: {
-            name: "Kimberly Smith",
-            action: "commented on your picture",
-            time: "1 week ago",
-            picture: "https://images.unsplash.com/photo-1606166187734-a4cb74079037?w=50&h=50&fit=crop"
-        }
+  },
+  {
+    img: "./assets/images/avatar-kimberly-smith.png",
+    info: {
+      name: "Kimberly Smith",
+      action: "commented on your picture",
+      postName: "",
+      time: "1 week ago",
+      privateMessage: "",
+      picture: "./assets/images/image-chess.png",
     },
-    {
-        img: "https://images.unsplash.com/photo-1527980965255-d3b416303d12?w=50&h=50&fit=crop&crop=face",
-        info: {
-            name: "Nathan Peterson",
-            action: "reacted to your recent post",
-            postName: "5 end-game strategies to increase your win rate",
-            time: "2 weeks ago"
-        }
+  },
+  {
+    img: "./assets/images/avatar-nathan-peterson.png",
+    info: {
+      name: "Nathan Peterson",
+      action: "reacted to your recent post",
+      postName: "5 end-game strategies to increase your win rate",
+      time: "2 weeks ago",
+      privateMessage: "",
+      picture: "",
     },
-    {
-        img: "https://images.unsplash.com/photo-1544725176-7c40e5a71c5e?w=50&h=50&fit=crop&crop=face",
-        info: {
-            name: "Anna Kim",
-            action: "left the group",
-            postName: "Chess Club",
-            time: "2 weeks ago"
-        }
-    }
+  },
+  {
+    img: "./assets/images/avatar-anna-kim.png",
+    info: {
+      name: "Anna Kim",
+      action: "left the group",
+      postName: "Chess Club",
+      time: "2 weeks ago",
+      privateMessage: "",
+      picture: "",
+    },
+  },
 ];
